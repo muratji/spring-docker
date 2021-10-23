4b4cd728a2f4e014b9b9284c243e61c6bf85c7d5
Murat J. Isidore <rmurat2@yahoo.fr>
2021-10-23 17:23:17 -0400

    First commit

---
+++ src/test/java/net/murati/springbootdocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.murati.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
